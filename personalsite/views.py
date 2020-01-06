090c8066948163106fe8496f87c8624f59b4f676
dicksonkariuki <dicksonkariuki4@gmail.com>
2020-01-06 10:21:48 +0300

    Add homepage introductory paragraph

--- personalsite/views.py
+++ personalsite/views.py
@@ -7,7 +7,7 @@ from django.http import Http404
 # Create your views here.
 def index(request):
     context={'classhome':"nav-current"}
-    return render(request,'templates/index.html',context)
+    return render(request,'index.html',context)
 
 def projects(request):
     context={'classprojects':"nav-current"}
